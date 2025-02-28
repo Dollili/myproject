cb478988d2c684869a15ec71a0b175fa3a25fa07
Dollili <ad____da@naver.com>
2025-02-28 18:00:58 +0900

    밑작업

--- back/src/main/java/org/example/Controller/HomeController.java
+++ back/src/main/java/org/example/Controller/HomeController.java
@@ -4,20 +4,27 @@ import org.example.Service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(value = "/board")
 public class HomeController {
 
     @Autowired
     private HomeService homeService;
 
-    @GetMapping("/home")
-    public List<Map<String, Object>> home() {
+    @GetMapping("")
+    public List<Map<String, Object>> board() {
         return homeService.getBoardList();
     }
+
+    @GetMapping("/detail")
+    public Map<String, Object> boardDetail(@RequestParam int no) {
+        return homeService.getBoardDetail(no);
+    }
+
 }
