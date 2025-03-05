47e1828f634059d50a7628ecd322f30fb4f1dd2d
Dollili <ad____da@naver.com>
2025-03-05 16:16:11 +0900

    글 검색, 게시글 삭제, 댓글 삭제 추가 + 사이드메뉴 예정

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import SideMenu from "./SideMenu";
 
 const Header = () => {
+  const [onOff, SetOn] = useState(true);
+
+  const close = () => {
+    SetOn(!onOff);
+  };
+
   return (
     <>
       <header className="App-header">
-        <p
-          className="logo"
-          onClick={() => {
-            window.location.href = "/";
-          }}
-        >
-          HOME
-        </p>
+        <div className="logo">
+          <p
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            홈화면
+          </p>
+        </div>
+        <p className="menu" onClick={() => close()}>메뉴</p>
       </header>
+      <SideMenu onOff={onOff}/>
     </>
   );
 };
