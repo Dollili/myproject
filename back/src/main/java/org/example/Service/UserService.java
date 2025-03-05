f4dffaba9accc2082d8ec447ddcbf7fc911f0aad
dikim <dikim@nglp.kr>
2025-03-05 17:54:59 +0900

    로그인 구현 중 ...

---
+++ back/src/main/java/org/example/Service/UserService.java
@@ -0,0 +1,28 @@
+package org.example.Service;
+
+import org.example.Repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Boolean userLogin(Map<String, Object> params) throws UsernameNotFoundException {
+        String user = userMapper.userLogin(params);
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found : " + params.get("id"));
+        }
+        return true;
+    }
+
+    public int register(Map<String, Object> params) {
+        return userMapper.insertUser(params);
+    }
+
+}
