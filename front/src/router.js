88a370eb24b6aec6d5dc046f8948b418bb604630
dikim <dikim@nglp.kr>
2025-03-06 17:30:46 +0900

    security...

--- front/src/router.js
+++ front/src/router.js
@@ -1,28 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import App from "./App";
 import NotFound from "./pages/NotFound";
 import Board from "./pages/board/Board";
 import BoardDetail from "./pages/board/BoardDetail";
 
 const router = createBrowserRouter(
-  [
+    [
+        {
+            path: "",
+            element: <App/>,
+            errorElement: <NotFound/>,
+        },
+        {
+            path: "board",
+            element: <Board/>,
+        },
+        {
+            path: "board/:no",
+            element: <BoardDetail/>,
+        },
+    ],
     {
-      path: "/",
-      element: <App />,
-      errorElement: <NotFound />,
+        basename: "/", // basename 명시적 설정
     },
-    {
-      path: "board",
-      element: <Board />,
-    },
-    {
-      path: "board/:no",
-      element: <BoardDetail />,
-    },
-  ],
-  {
-    basename: "/", // basename 명시적 설정
-  },
 );
 
 export default router;
