67cc6ba77356fd188ba130739e2977188cb44751
Dollili <ad____da@naver.com>
2025-03-12 15:36:37 +0900

    시큐리티 및 리액트 라우팅 관련

--- back/src/main/java/org/example/Controller/LoginController.java
+++ back/src/main/java/org/example/Controller/LoginController.java
@@ -1,8 +1,8 @@
 package org.example.Controller;
 
 
+import lombok.RequiredArgsConstructor;
 import org.example.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +10,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping("")
-    public ResponseEntity<Void> login(@RequestBody Map<String, Object> params) {
-        Boolean result = userService.userLogin(params);
-        if (result) {
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    public ResponseEntity<?> login(@RequestParam Map<String, Object> params) {
+        Map<String, Object> result = userService.userLogin(params);
+        if (result != null) {
+            result.remove("USER_PWD");
         }
+        
+        return ResponseEntity.ok(result);
     }
 
-    @PostMapping("/register")
-    public int register(@RequestBody Map<String, Object> params) {
-        return userService.register(params);
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody Map<String, Object> params) {
+        try {
+            if (params.get("pwd") == null) {
+                return ResponseEntity.badRequest().body("password is null");
+            }
+
+            Map<String, Object> user = userService.join(params);
+            if (user == null) {
+                return ResponseEntity.internalServerError()
+                        .body("cannot register");
+            }
+
+            HttpStatus status = user.get("duplicate") != null && !((boolean) user.get("duplicate")) ? HttpStatus.OK : HttpStatus.CREATED;
+            return ResponseEntity.status(status).body(user);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
--- front/src/App.js
+++ front/src/App.js
@@ -1,76 +1,19 @@
 import "./App.css";
-import React, {useEffect, useState} from "react";
-import {Button} from "react-bootstrap";
-import {Link, useNavigate} from "react-router-dom";
+import React from "react";
+import {Outlet} from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
-function App() {
-    const nav = useNavigate();
-    const [user, setUser] = useState({});
-    const [check, setCheck] = useState(false);
-
-    const userObj = (e) => {
-        const {name, value} = e.target;
-        setUser({...user, [name]: value});
-    };
-
-    const login = (type) => {
-        if (type) {
-            user["role"] = "M";
-        } else {
-            user["role"] = "U";
-        }
-        console.log(user)
-        nav('board');
-    };
-
-    useEffect(() => {
-    }, [check, user]);
-
+const App = () => {
     return (
-        <div className="App-contents">
-            <div className="login">
-                <div className="login-Form">
-                    <input
-                        name="userId"
-                        value={userObj.userId || ""}
-                        placeholder="id"
-                        onChange={(e) => {
-                            userObj(e);
-                        }}
-                    />
-                    <input
-                        type="password"
-                        name="pwd"
-                        value={userObj.pwd || ""}
-                        placeholder="password"
-                        onChange={(e) => {
-                            userObj(e);
-                        }}
-                    />
-                </div>
-                <Button
-                    onClick={() => {
-                        login(check);
-                    }}
-                >
-                    로그인
-                </Button>
+        <div className="App">
+            <Header/>
+            <div className="App-main">
+                <Outlet/>
             </div>
-            <div className="choice-Form">
-                <Link to={"/board"}>회원가입</Link>
-                <label>
-                    관리자
-                    <input
-                        type="checkbox"
-                        checked={check}
-                        onChange={() => {
-                            setCheck(!check);
-                        }}
-                    />
-                </label>
-            </div>
+            <Footer/>
         </div>
     );
-}
+};
 
 export default App;
