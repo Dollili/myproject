f3ad9f3b3a342ea99a3c79aafd76a1439e96d7aa
Dollili <ad____da@naver.com>
2025-03-12 15:36:37 +0900

    시큐리티 및 리액트 라우팅 관련

--- front/src/router.js
+++ front/src/router.js
@@ -3,26 +3,30 @@ import App from "./App";
 import NotFound from "./pages/NotFound";
 import Board from "./pages/board/Board";
 import BoardDetail from "./pages/board/BoardDetail";
+import ProtectedRoute from "./ProtectedRoute";
+import Register from "./pages/user/Register";
+import Home from "./pages/Home";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "",
-            element: <App/>,
-            errorElement: <NotFound/>,
-        },
-        {
-            path: "board",
-            element: <Board/>,
-        },
-        {
-            path: "board/:no",
-            element: <BoardDetail/>,
-        },
-    ],
+const router = createBrowserRouter([
     {
-        basename: "/", // basename 명시적 설정
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound/>,
+        children: [
+            {path: "", element: <Home/>},
+            {path: "register", element: <Register/>},
+            {
+                path: "board",
+                element: <ProtectedRoute/>, // 보호된 경로 그룹
+                children: [
+                    {
+                        index: true,
+                        element: <Board/>,
+                    },
+                    {path: ":no", element: <BoardDetail/>},
+                ],
+            },
+        ],
     },
-);
-
+]);
 export default router;
