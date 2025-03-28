dc6865d23b28a12a5f69588581b596e3604eadb3
dikim <dikim@nglp.kr>
2025-03-28 14:01:27 +0900

    메뉴, 파일업로드 구현중 ...

---
+++ back/src/main/java/org/example/util/CryptoUtil.java
@@ -0,0 +1,26 @@
+package org.example.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class CryptoUtil {
+    public static String encryptSHA256(String str) {
+        try {
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            byte[] hash = sha256.digest(str.getBytes());
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 암호화 오류", e);
+        }
+    }
+}
--- front/src/components/SideMenu.jsx
+++ front/src/components/SideMenu.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "../assets/css/menu.css";
 
-const SideMenu = ({ onOff }) => {
-  return (
-    <div className="App-side">
-      <div className={`dropdown ${onOff ? "active" : ""}`}>
-
-      </div>
-    </div>
-  );
+const SideMenu = ({onOff}) => {
+    return (
+        <div className="App-side">
+            <div className={`dropdown ${onOff ? "active" : ""}`}>
+                <div>공지사항</div>
+                <div>QnA</div>
+                <div>금주 랭킹</div>
+                <div>사용자</div>
+            </div>
+        </div>
+    );
 };
 
 export default SideMenu;
