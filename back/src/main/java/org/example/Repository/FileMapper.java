197ae6775389c5325ec8555e3bb93bdd493accb5
Dollili <ad____da@naver.com>
2025-04-03 09:43:11 +0900

    파일 업로드 추가

--- back/src/main/java/org/example/Repository/FileMapper.java
+++ back/src/main/java/org/example/Repository/FileMapper.java
@@ -1,11 +1,22 @@
 package org.example.Repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface FileMapper {
 
-    int insertFile(Map<String, Object> file);
+    void insertFile(Map<String, Object> file);
+
+    List<Map<String, Object>> getFileList(String no);
+
+    @Update("UPDATE FILE SET DEL_YN = 'Y' WHERE ID = #{id}")
+    void deleteFile(String id);
+
+    @Update("UPDATE FILE SET DEL_YN = 'Y' WHERE BOARD_NO = #{no}")
+    void deleteAll(String no);
+
 }
