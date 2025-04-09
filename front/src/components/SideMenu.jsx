8e27a899c468ecd9ce327841c1e32c7bdd58794e
dikim <dikim@nglp.kr>
2025-04-09 14:07:17 +0900

    url 변경, 카테고리 컬럼 추가(공지/일반글), 사용자 통계는 관리자만

--- front/src/components/SideMenu.jsx
+++ front/src/components/SideMenu.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "../styles/css/menu.css";
 import {useNavigate} from "react-router-dom";
+import {UserContext} from "../contexts/UserContext";
 
 const SideMenu = ({onOff}) => {
     const nav = useNavigate();
+    const {user, setUser} = useContext(UserContext);
+    const [role, setRole] = useState(false);
+
+    useEffect(() => {
+        if (user) {
+            user.ROLE === "M" ? setRole(true) : setRole(false);
+        }
+    }, []);
 
     return (
         <div className="App-side">
             <div className={`dropdown ${onOff ? "active" : ""}`}>
-                <div className="menu-item" onClick={() => nav("/notice")}>공지사항</div>
-                <div className="menu-item" onClick={() => nav("/board")}>게시판</div>
-                <div className="menu-item">금주 랭킹</div>
-                <div className="menu-item">사용자</div>
-                <div className="menu-item">QnA</div>
+                <div className="menu-item" onClick={() => nav("/notice")}>
+                    공지사항
+                </div>
+                <div className="menu-item" onClick={() => nav("/board")}>
+                    게시판
+                </div>
+                <div className="menu-item" onClick={() => nav("/ranking")}>금주 랭킹</div>
+                {role && <div className="menu-item" onClick={() => nav("/userBoard")}>사용자 통계</div>}
+                <div className="menu-item" onClick={() => nav("/qna")}>QnA</div>
             </div>
         </div>
     );
