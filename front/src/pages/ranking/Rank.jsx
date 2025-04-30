4407e7d5d5739f029aa584e12db600f97526e7ef
dikim <dikim@nglp.kr>
2025-04-30 14:23:00 +0900

    레디스 활용 조회수 count

--- front/src/pages/ranking/Rank.jsx
+++ front/src/pages/ranking/Rank.jsx
@@ -1,34 +1,94 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import medal2 from "../../assets/img/free-icon-medal-2583319.png";
+import medal3 from "../../assets/img/free-icon-medal-2583434.png";
+import medal1 from "../../assets/img/free-icon-medal-2583344.png";
+import noImg from "../../assets/img/free-icon-broken-image-13435075.png";
+
+import {dbGet} from "../../services/commonApi";
 
 const Rank = () => {
-    const [data, setData] = useState([]);
+    const [data1, setData1] = useState({});
+    const [data2, setData2] = useState({});
+    const [data3, setData3] = useState({});
+    const [load, setLoad] = useState(false);
+
+    const getRanking = async () => {
+        const res = await dbGet("/board/imgList/rank", {});
+        if (res.length === 0) {
+            setLoad(true);
+            return;
+        }
+        if (res[0]?.IMG_PATH) {
+            setData1(res[0]);
+        }
+        if (res[1]?.IMG_PATH) {
+            setData2(res[1]);
+        }
+        if (res[2]?.IMG_PATH) {
+            setData3(res[2]);
+        }
+    };
+
+    useEffect(() => {
+        getRanking();
+    }, []);
 
     return (
-        <div className="main-container">
+        <div className="main-container ranking-container">
             <h2 className="boardTitle">명예의 전당</h2>
-            <div className="grid-container">
-                {data.length === 0 ? (
+            <div className="grid-container ranking">
+                {load ? (
                     <div className="noPost">RANKING 데이터가 존재하지 않습니다.</div>
                 ) : (
-                    data.map((item, index) => (
-                        <div
-                            key={index}
-                            className="card"
-                            onClick={() => {
-                            }}
-                        >
-                            <img src={`${process.env.REACT_APP_BACKEND_URL}${item.IMG_PATH}`} alt="thumbnail"/>
-                            <div className="info">
-                                <h4>{item.TITLE} <span
-                                    style={{color: "red", marginLeft: "3px"}}>[{item.COMMENT_CNT}]</span></h4>
-                                <p>{item.AUTHOR}</p>
-                                <p>{item.APPLY_FORMAT_DATE}</p>
-                                <p>추천 {item.RECOMMEND}</p>
-                                <p>조회수 {item.VIEW_CNT}</p>
+                    <>
+                        {/*<div className="left">왼쪽</div>*/}
+                        <div className="rank rank-2">
+                            <img className="crown" src={medal2} alt="crown"/>
+                            <div className="card rank2">
+                                <img
+                                    className={!data2.IMG_PATH ? "noImg" : ""}
+                                    src={
+                                        data2.IMG_PATH
+                                            ? `${process.env.REACT_APP_BACKEND_URL}${data2.IMG_PATH}`
+                                            : noImg
+                                    }
+                                    alt="thumbnail"
+                                />
+                                <div hidden={data2.IMG_PATH}>Image not found . . .</div>
                             </div>
-                            {" "}
                         </div>
-                    ))
+                        <div className="rank rank-1">
+                            <img className="crown" src={medal1} alt="crown"/>
+                            <div className="card rank1">
+                                <img
+                                    className={!data1.IMG_PATH ? "noImg" : ""}
+                                    src={
+                                        data1.IMG_PATH
+                                            ? `${process.env.REACT_APP_BACKEND_URL}${data1.IMG_PATH}`
+                                            : noImg
+                                    }
+                                    alt="thumbnail"
+                                />
+                                <div hidden={data1.IMG_PATH}>Image not found . . .</div>
+                            </div>
+                        </div>
+                        <div className="rank rank-3">
+                            <img className="crown" src={medal3} alt="crown"/>
+                            <div className="card rank3">
+                                <img
+                                    className={!data3.IMG_PATH ? "noImg" : ""}
+                                    src={
+                                        data3.IMG_PATH
+                                            ? `${process.env.REACT_APP_BACKEND_URL}${data3.IMG_PATH}`
+                                            : noImg
+                                    }
+                                    alt="thumbnail"
+                                />
+                                <div hidden={data3.IMG_PATH}>Image not found . . .</div>
+                            </div>
+                        </div>
+                        {/*<div className="right">오른쪽</div>*/}
+                    </>
                 )}
             </div>
         </div>
