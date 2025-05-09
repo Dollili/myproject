9ff86ed9d93e6e82a0ae786e068960b38c4cd54b
dikim <dikim@nglp.kr>
2025-05-09 15:23:34 +0900

    회원탈퇴 softdel

--- front/src/components/alert/customAlert.js
+++ front/src/components/alert/customAlert.js
@@ -1,4 +1,5 @@
 import {toast} from "react-toastify";
+import React from "react";
 
 export const customAlert = (msg, callback) => {
     alert(msg);
@@ -8,3 +9,31 @@ export const customAlert = (msg, callback) => {
 export const showAlert = (msg) => {
     toast.warn(msg);
 };
+
+export const deleteAlert = (msg, func, no) => {
+    const toastId = toast.info(
+        <div className="remainInfo">
+            <div>{msg}</div>
+            <button
+                className="remainTime-btn remainLogin"
+                onClick={() => {
+                    if (no) {
+                        func(no);
+                    } else {
+                        func();
+                    }
+                    toast.dismiss(toastId);
+                }}
+            >
+                확인
+            </button>
+            <button
+                className="remainTime-btn"
+                onClick={() => toast.dismiss(toastId)}
+            >
+                닫기
+            </button>
+        </div>,
+        {autoClose: false},
+    )
+}
