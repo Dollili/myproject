2dbd598ed097022148be0de58460353aa761b591
Dollili <ad____da@naver.com>
2025-05-19 11:10:55 +0900

    cors -> securityconfig 파일에 합치기

--- back/src/main/java/org/example/Config/SecurityConfig.java
+++ back/src/main/java/org/example/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.Filter.JwtAuthenticationFilter;
 import org.example.Service.UserDetailService;
 import org.example.util.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,16 +19,23 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.NullSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CorsConfig corsConfig;
-    //private final SessionFilter sessionFilter;
+    //private final CorsConfig corsConfig;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserDetailService userDetailService;
+
+    @Value("${spring.graphql.cors.allowedOrigins}")
+    private String allowedOrigins;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -37,6 +45,22 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigins); // 허용할 URL
+        config.addAllowedHeader("*"); // 허용할 Header
+        config.addAllowedMethod("*"); // 허용할 Http Method
+
+        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type", "Content-Disposition"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
@@ -56,7 +80,7 @@ public class SecurityConfig {
                         .hasAnyRole("U", "M")
                         .anyRequest()
                         .authenticated())
-                .addFilterBefore(corsConfig.corsFilter(), SecurityContextHolderFilter.class)
+                .addFilterBefore(corsFilter(), SecurityContextHolderFilter.class)
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userDetailService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
